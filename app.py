c53e047f2320a1af8261418e00920af5d2a0a659
soloautonomos <asesoriasoloautonomos@gmail.com>
2020-12-23 14:55:44 +0100

    remove useless libraries

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, url_for
-import os, json   
+import os   
 app = Flask(__name__)
-
-import os
 
 import numpy as np
 from sklearn.model_selection import train_test_split
